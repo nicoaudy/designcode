aa4b856950a61a5048a6a62c0b7f6dd9b23833e4
Nico Audy <nicoaudy1994@gmail.com>
2020-11-27 10:16:03 +0700

    update icons for ios spesific using platform check

--- lib/models/sidebar.dart
+++ lib/models/sidebar.dart
@@ -1,7 +1,10 @@
+import 'dart:io';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class SidebarItem {
-  SidebarItem({ this.title, this.background, this.icon });
+  SidebarItem({this.title, this.background, this.icon});
 
   String title;
   LinearGradient background;
@@ -12,49 +15,43 @@ var sidebarItems = [
   SidebarItem(
     title: "Home",
     background: LinearGradient(
-        begin: Alignment.topLeft,
-        end: Alignment.bottomRight,
-        colors: [
-          Color(0xFF00AEFF),
-          Color(0xFF0076FF)
-        ]
+      begin: Alignment.topLeft,
+      end: Alignment.bottomRight,
+      colors: [Color(0xFF00AEFF), Color(0xFF0076FF)],
     ),
-    icon: Icon(Icons.home, color: Colors.white)
+    icon: Icon(Icons.home, color: Colors.white),
   ),
   SidebarItem(
-      title: "Courses",
-      background: LinearGradient(
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
-          colors: [
-            Color(0xFFFA7D75),
-            Color(0xFFC23D61)
-          ]
-      ),
-      icon: Icon(Icons.library_books, color: Colors.white)
+    title: "Courses",
+    background: LinearGradient(
+      begin: Alignment.topLeft,
+      end: Alignment.bottomRight,
+      colors: [Color(0xFFFA7D75), Color(0xFFC23D61)],
+    ),
+    icon: Icon(
+      Platform.isAndroid ? Icons.library_books : CupertinoIcons.book_solid,
+      color: Colors.white,
+    ),
   ),
   SidebarItem(
-      title: "Billing",
-      background: LinearGradient(
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
-          colors: [
-            Color(0xFFFAD64A),
-            Color(0xFFEA880F)
-          ]
-      ),
-      icon: Icon(Icons.credit_card, color: Colors.white)
+    title: "Billing",
+    background: LinearGradient(
+      begin: Alignment.topLeft,
+      end: Alignment.bottomRight,
+      colors: [Color(0xFFFAD64A), Color(0xFFEA880F)],
+    ),
+    icon: Icon(Icons.credit_card, color: Colors.white),
   ),
   SidebarItem(
-      title: "Setting",
-      background: LinearGradient(
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
-          colors: [
-            Color(0xFF4E62CC),
-            Color(0xFF202A78)
-          ]
-      ),
-      icon: Icon(Icons.settings, color: Colors.white)
+    title: "Setting",
+    background: LinearGradient(
+      begin: Alignment.topLeft,
+      end: Alignment.bottomRight,
+      colors: [Color(0xFF4E62CC), Color(0xFF202A78)],
+    ),
+    icon: Icon(
+      Platform.isAndroid ? Icons.settings : CupertinoIcons.settings_solid,
+      color: Colors.white,
+    ),
   ),
 ];
